bab701ea0b20320c3c64d0058b7989e44528ca6a
Javier Gil <javier.gil@module-7.com>
2016-08-11 17:25:39 +0200

    Support for the password change in the web service

--- EventListener/UserEventsSubsbriber.php
+++ EventListener/UserEventsSubsbriber.php
@@ -8,6 +8,7 @@ use FOS\UserBundle\Event\FormEvent;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
 use EHEncryptionBundle\Service\EncryptionService;
 use EHEncryptionBundle\Entity\PKEncryptionEnabledUserInterface;
+use AppWebServiceBundle\Event as WebServiceEvent;
 
 class UserEventsSubsbriber implements EventSubscriberInterface
 {
@@ -27,7 +28,8 @@ class UserEventsSubsbriber implements EventSubscriberInterface
             FOSUserEvents::CHANGE_PASSWORD_SUCCESS => 'handlePasswordChangeSuccess',
             FOSUserEvents::RESETTING_RESET_SUCCESS => 'handlePasswordResetSuccess',
             FOSUserEvents::REGISTRATION_SUCCESS => 'handleUserRegistrationSuccess',
-            FOSUserEvents::REGISTRATION_COMPLETED => 'handleUserRegistrationComplete'
+            FOSUserEvents::REGISTRATION_COMPLETED => 'handleUserRegistrationComplete',
+            WebServiceEvent\Events::PV_WS_PASSWORD_CHANGE_SUCCESS => 'handleWebServicePasswordChangeSuccess',
         );
 
         return $events;
@@ -69,6 +71,17 @@ class UserEventsSubsbriber implements EventSubscriberInterface
 
         if ($user instanceof PKEncryptionEnabledUserInterface) {
             $this->encryptionService->handleUserRegistrationComplete($user);
+        }
+    }
+
+    public function handleWebServicePasswordChangeSuccess(WebServiceEvent\UserEvent $event)
+    {
+        $user = $event->getUser();
+
+        if ($user instanceof PKEncryptionEnabledUserInterface) {
+            // By now in the app the current password is not sent when the user wants to
+            // change his password, so we have to simulate a password reset
+            $this->encryptionService->handleUserPasswordResetSuccess($user);
         }
     }
 }
